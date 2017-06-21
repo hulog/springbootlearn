a4e64831ae31bdfa412004553c4586c4bfcb22f0
norman <450382690@qq.com>
2017-06-21 17:36:43 +0800

    调整GetterService

--- src/main/java/com/fuckSpring/config/RabbitConfig.java
+++ src/main/java/com/fuckSpring/config/RabbitConfig.java
@@ -2,6 +2,7 @@ package com.fuckSpring.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +14,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
+    private String queueName;
+    private String routingKey;
+    private String exchangeName;
+
     @Bean
     public Queue getQueue() {
         return new Queue("Ip_Proxy_Queue", true);
+    }
+
+    @Bean
+    public DirectExchange getExchange() {
+        return new DirectExchange(exchangeName, false, false);
+    }
+
+    @Bean
+    public Binding getBinding() {
+        return new Binding(queueName, Binding.DestinationType.EXCHANGE, exchangeName, routingKey, null);
     }
 }
