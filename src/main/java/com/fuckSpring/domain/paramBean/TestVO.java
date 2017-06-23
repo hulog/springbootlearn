8eabe1e8c04ad18937f7983a575ab4169b506a36
norman <450382690@qq.com>
2017-06-23 16:27:21 +0800

    调整GetterService

---
+++ src/main/java/com/fuckSpring/domain/paramBean/TestVO.java
@@ -0,0 +1,25 @@
+package com.fuckSpring.domain.paramBean;
+
+/**
+ * Created by upsmart on 17-6-19.
+ */
+public class TestVO {
+    private String[] address;
+    private Integer bonus;
+
+    public String[] getAddress() {
+        return address;
+    }
+
+    public void setAddress(String[] address) {
+        this.address = address;
+    }
+
+    public Integer getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Integer bonus) {
+        this.bonus = bonus;
+    }
+}
