2a5196296fa47cbc2262d24f566139ad37ffe221
hulong <450382690@qq.com>
2017-08-08 01:02:37 +0800

    完成GET请求的异步封装

--- src/main/java/com/fuckSpring/util/okhttp/OkHttpUtils.java
+++ src/main/java/com/fuckSpring/util/okhttp/OkHttpUtils.java
@@ -7,75 +7,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 进度:
+ * 1. 完成同步Get请求
+ * 2. 完成异步Get请求
+ * 3. (未)完成同步Post表单请求
+ * 4. (未)完成异步Post表单请求
+ * 5. (未)完成同步PostJSON请求
+ * 6. (未)完成异步PostJSON请求
+ *
+ * @Author: norman
+ */
 public final class OkHttpUtils {
 
     // 使用默认配置
+    // 注意：dispatcher中维护了一个线程池，限制了异步请求数：同一主机一次最多5个请求；若不同主机，并发限制在64个
+    // 可进行修改，类似于client.dispatcher().setMaxRequestsPerHost()或client.dispatcher().setMaxRequests();
     private static final OkHttpClient client = new OkHttpClient.Builder()
             .readTimeout(20, TimeUnit.SECONDS).build();
 
     private OkHttpUtils() {
-    }
-
-
-    // 1. 组装方法
-    // switch (method) {
-    //     case Method.GET: {
-    //         builder.get();
-    //         if (paramsMap != null && paramsMap.size() > 0) {
-    //             StringBuilder sb = new StringBuilder();
-    //             for (Map.Entry<String, String> entry : paramsMap.entrySet()) {
-    //                 sb.append()
-    //             }
-    //         }
-    //     }
-    //     case Method.POST: {
-    //         FormBody.Builder formBuild = new FormBody.Builder();
-    //         for (Map.Entry<String, String> entry : paramsMap.entrySet()) {
-    //             formBuild.add(entry.getKey(), entry.getValue());
-    //         }
-    //         FormBody body = formBuild.build();
-    //         builder.post(body);
-    //     }
-    //     default: {
-    //         builder.get();
-    //     }
-    // }
-    // // 2. 组装URL
-    // builder.url(url);
-    // // 3. 组装header
-    // if (headersMap != null || headersMap.size() > 0) {
-    //     for (Map.Entry<String, String> entry : headersMap.entrySet()) {
-    //         builder.addHeader(entry.getKey(), entry.getValue());
-    //     }
-    // }
-    // return builder.build();
-// }
-
-
-    public static void getAsync(String url, Callback callback) {
-        getAsync(url, null, null, callback);
-    }
-
-    public static void getAsync(String url, Map<String, String> paramsMap, Callback callback) {
-        getAsync(url, paramsMap, null, callback);
-    }
-
-    public static void getAsync(String url, Map<String, String> paramsMap, Map<String, String> headersMap, Callback callback) {
-
-        // 0. 创建新请求
-        Request.Builder reqBuilder = new Request.Builder();
-
-        // 1. 确定请求方式GET
-        reqBuilder.get();
-
-        // 2. 组装URL
-        String urlParams = makeGetParams(paramsMap);
-        reqBuilder.url(urlParams == null ? url : url + urlParams);
-
-        // 3. 组装Header
-        makeHeaders(reqBuilder, headersMap);
-
-        client.newCall(reqBuilder.build()).enqueue(callback);
     }
 
     public static void post(String url, Callback callback) {
@@ -96,40 +47,6 @@ public final class OkHttpUtils {
 
     public static void postJson(String url, String jsonStr, Map<String, String> headersMap, Callback callback) {
         //TODO
-    }
-
-    private static Request makeGetRequest(String url, Map<String, String> paramsMap, Map<String, String> headersMap) {
-
-        Request.Builder builder = new Request.Builder();
-
-        // 1. 确定请求方式GET
-        builder.get();
-
-        // 2. 组装URL
-        if (null == paramsMap && paramsMap.size() < 1) {
-            builder.url(url);
-        } else {
-            StringBuilder sb = new StringBuilder(url);
-            sb.append("?");
-            for (Map.Entry<String, String> entry : paramsMap.entrySet()) {
-                sb.append(entry.getKey())
-                        .append("=")
-                        .append(entry.getValue())
-                        .append("&");
-            }
-            builder.url(sb.substring(0, sb.length() - 1));
-        }
-
-        // 3. 组装Header
-        if (headersMap != null && headersMap.size() > 0) {
-            StringBuilder sb = new StringBuilder();
-            for (Map.Entry<String, String> entry : headersMap.entrySet()) {
-                builder.addHeader(entry.getKey(), entry.getValue());
-            }
-        }
-
-        // 4. 组装完毕返回
-        return builder.build();
     }
 
     private static class Method {
@@ -173,11 +90,20 @@ public final class OkHttpUtils {
         return sb.substring(0, sb.length() - 1);
     }
 
+    /**
+     * 发起同步GET请求
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
     public static Response get(String url) throws IOException {
         return get(url, null, null);
     }
 
     /**
+     * 发起同步GET请求
+     *
      * @param url
      * @param paramsMap
      * @return
@@ -219,6 +145,53 @@ public final class OkHttpUtils {
         return client.newCall(reqBuilder.build()).execute();
     }
 
+    /**
+     * 发起异步GET请求
+     *
+     * @param url      请求地址
+     * @param callback
+     */
+    public static void getAsyc(String url, Callback callback) {
+        getAsyc(url, null, null, callback);
+    }
+
+    /**
+     * 发起异步GET请求
+     *
+     * @param url       请求地址
+     * @param paramsMap 请求参数
+     * @param callback
+     */
+    public static void getAsyc(String url, Map<String, String> paramsMap, Callback callback) {
+        getAsyc(url, paramsMap, null, callback);
+    }
+
+    /**
+     * 发起异步GET请求
+     *
+     * @param url        请求地址
+     * @param paramsMap  请求参数
+     * @param headersMap 请求头参数
+     * @param callback
+     */
+    public static void getAsyc(String url, Map<String, String> paramsMap, Map<String, String> headersMap, Callback callback) {
+
+        // 0. 创建新请求
+        Request.Builder reqBuilder = new Request.Builder();
+
+        // 1. 确定请求方式GET
+        reqBuilder.get();
+
+        // 2. 组装URL
+        String urlParams = makeGetParams(paramsMap);
+        reqBuilder.url(urlParams == null ? url : url + urlParams);
+
+        // 3. 组装Header
+        makeHeaders(reqBuilder, headersMap);
+
+        client.newCall(reqBuilder.build()).enqueue(callback);
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException {
         // HttpsUtils.SSLParams sslSocketFactory = HttpsUtils.getSslSocketFactory(null, null, null);
         // OkHttpClient ok = new OkHttpClient.Builder()
@@ -255,7 +228,7 @@ public final class OkHttpUtils {
         head.put("user-agent", "IDEA;chrome");
         for (int i = 0; i < 10; i++) {
 
-            OkHttpUtils.getAsync("https://www.baidu.com", new Callback() {
+            OkHttpUtils.getAsyc("https://www.baidu.com", new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
                     System.out.println("访问百度失败咯");
@@ -263,12 +236,12 @@ public final class OkHttpUtils {
 
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
-                    System.out.println(Thread.currentThread().getName() + "访问百度成功:"+response.code());
+                    System.out.println(Thread.currentThread().getName() + "访问百度成功:" + response.code());
                 }
             });
 
 
-            OkHttpUtils.getAsync("http://localhost:8999/oktest", null, head, new Callback() {
+            OkHttpUtils.getAsyc("http://localhost:8999/oktest", null, head, new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
                     System.out.println("失败咯");
@@ -276,7 +249,7 @@ public final class OkHttpUtils {
 
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
-                    System.out.println(Thread.currentThread().getName()+"[客户端]:" + response.body().string());
+                    System.out.println(Thread.currentThread().getName() + "[客户端]:" + response.body().string());
                 }
             });
         }
